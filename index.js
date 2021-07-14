b74fe7d7a983b79bde00a73e266736a5e858a0ae
Soumik Datta <mailmeiamsa@gmail.com>
2021-07-15 02:42:55 +0545

    save passwords to .txt file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const program = require('commander')
 const chalk = require('chalk')
 const clipboardy = require('clipboardy')
 const createPassword = require('./utils/createPassword')
+const savePassword = require('./utils/savePassword')
 const log = console.log
 
 program.version('1.0.0').description('Simple Password Generator')
@@ -17,6 +18,11 @@ const { length, save, numbers, symbols } = program.opts()
 
 // Get generated password
 const generatedPassword = createPassword(length, numbers, symbols)
+
+// Save to file 
+if(save){
+    savePassword(generatedPassword)
+}
 
 // Copy to clipboard
 clipboardy.writeSync(generatedPassword)
